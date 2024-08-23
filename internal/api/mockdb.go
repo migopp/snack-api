7af63daf4c469ac31571d2bb89296eaf410cf14f
Michael Goppert <goppert@utexas.edu>
2024-08-23 14:42:35 -0500

    Refactor mock db into map structure

--- internal/api/mockdb.go
+++ internal/api/mockdb.go
@@ -1,39 +1,40 @@
 package api
 
 type MockStore struct {
-	users []Snacker
+	snackers    map[uint64]*Snacker
+	numSnackers uint64
 }
 
 func CreateMockStore() *MockStore {
 	return &MockStore{
-		[]Snacker{
-			{
+		map[uint64]*Snacker{
+			0: {
 				ID:        0,
 				FirstName: "Michael",
 				LastName:  "Goppert",
 				Snacks:    []string{"じゃがりこ"},
 				Hearts:    6,
 			},
-			{
+			1: {
 				ID:        1,
 				FirstName: "Dylan",
 				LastName:  "Horton",
 				Snacks:    []string{"Goldfish", "Braum's Cheeseburger"},
 				Hearts:    5,
 			},
-			{
+			2: {
 				ID:        2,
 				FirstName: "Jason",
 				LastName:  "Chavez",
 				Snacks:    []string{"Dust"},
 				Hearts:    0,
 			},
-		},
+		}, 3,
 	}
 }
 
 func (m *MockStore) CreateSnacker(s *SnackerRegistration) (uint64, error) {
-	id := uint64(len(m.users))
+	id := m.numSnackers
 	snacker := &Snacker{
 		ID:        id,
 		FirstName: s.FirstName,
@@ -41,32 +42,34 @@ func (m *MockStore) CreateSnacker(s *SnackerRegistration) (uint64, error) {
 		Snacks:    s.Snacks,
 		Hearts:    0,
 	}
-	m.users = append(m.users, *snacker)
+	m.snackers[id] = snacker
+	m.numSnackers += 1
 	return id, nil
 }
 
 func (m *MockStore) DeleteSnacker(id uint64) error {
-	m.users[id] = Snacker{}
+	delete(m.snackers, id)
 	return nil
 }
 
 func (m *MockStore) UpdateSnacker(s *Snacker) error {
-	m.users[s.ID] = *s
+	m.snackers[s.ID] = s
 	return nil
 }
 
 func (m *MockStore) FindSnacker(id uint64) (*Snacker, error) {
-	if id >= uint64(len(m.users)) {
+	snacker, ok := m.snackers[id]
+	if !ok {
 		return &Snacker{}, nil
 	}
 
-	return &m.users[id], nil
+	return snacker, nil
 }
 
 func (m *MockStore) FetchSnackers() ([]*Snacker, error) {
 	var snackers []*Snacker
-	for _, snacker := range m.users {
-		snackers = append(snackers, &snacker)
+	for _, snacker := range m.snackers {
+		snackers = append(snackers, snacker)
 	}
 	return snackers, nil
 }
